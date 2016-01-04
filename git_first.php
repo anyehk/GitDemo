c1f78c88d5d0f46bd8c36a96d166d9cb67a62e8f
anyehk <anyehk@163.com>
2016-01-04 22:51:25 +0800

    How to use

--- git_first.php
+++ git_first.php
@@ -1,8 +1,8 @@
 <?php
-echo "my first";
-echo "my second";
-echo "3333333";
-#从github上修改数据
-#收到从github上同步的数据到本地，
+#Learn to use GitHub
 
-#从本地发布到github
+#Net too slow
+
+#China limits foreign networks
+
+#.... How to use
