e70309310d222092deab2a0f67b9258e16c5080c
anyehk <anyehk@163.com>
2016-01-04 23:12:20 +0800

    结论：提交到github前必须先提交到本地git

--- git_first.php
+++ git_first.php
@@ -10,3 +10,15 @@
 #I might have learned a little.
 
 #I have a problem
+
+#maybe...
+
+#test too...
+
+#failed
+
+#test too...
+
+#Conclusion: submitted to the GitHub must be submitted to the local Git
+
+#额，...或许真的是这样，我再试试...
